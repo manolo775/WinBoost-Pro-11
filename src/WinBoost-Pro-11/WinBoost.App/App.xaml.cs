ac6d90bb17e20430fcec03e577a71b07996c525d
manolo775 <mariandobre2000@yahoo.com>
2026-07-23 14:57:24 +0300

    Initial WinBoost Pro 11 project structure

---
+++ src/WinBoost-Pro-11/WinBoost.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WinBoost.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
