29974b93ff2bfec46f2bff4e7454d59d0d0bf3e0
manolo775 <mariandobre2000@yahoo.com>
2026-07-24 18:46:03 +0300

    Sprint 1 - Initial commercial UI and theme completed

---
+++ src/WinBoost-Pro-11/WinBoost.App/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WinBoost.App.ViewModels
+{
+    public class DashboardViewModel : INotifyPropertyChanged
+    {
+        private string _cpuUsage = "0 %";
+
+        public string CpuUsage
+        {
+            get => _cpuUsage;
+            set
+            {
+                _cpuUsage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(
+            [CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ src/WinBoost-Pro-11/WinBoost.App/Views/DashboardView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using WinBoost.App.ViewModels;
+
+namespace WinBoost.App.Views
+{
+    public partial class DashboardView : UserControl
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+
+            DataContext = new DashboardViewModel();
+        }
+    }
+}
