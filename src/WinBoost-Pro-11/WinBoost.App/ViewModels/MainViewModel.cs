097669b3ab3511aac4f68f0797f46710318e8ea4
manolo775 <mariandobre2000@yahoo.com>
2026-07-24 19:22:49 +0300

    Sprint 2 - Added MainViewModel and connected MainWindow

---
+++ src/WinBoost-Pro-11/WinBoost.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WinBoost.App.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _currentPage = "Dashboard";
+
+        public string CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(
+            [CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
