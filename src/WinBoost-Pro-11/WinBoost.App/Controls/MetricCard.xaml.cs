97cc54c6569aeea2ec9407d5a2a4193997c3178d
manolo775 <mariandobre2000@yahoo.com>
2026-07-25 19:53:10 +0300

    feat: replace dashboard cards with reusable MetricCard control

---
+++ src/WinBoost-Pro-11/WinBoost.App/Controls/MetricCard.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WinBoost.App.Controls
+{
+    /// <summary>
+    /// Interaction logic for MetricCard.xaml
+    /// </summary>
+    public partial class MetricCard : UserControl
+    {
+        public MetricCard()
+        {
+            InitializeComponent();
+        }
+        public static readonly DependencyProperty TitleProperty =
+    DependencyProperty.Register(
+        nameof(Title),
+        typeof(string),
+        typeof(MetricCard),
+        new PropertyMetadata("Metric"));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+        public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register(
+        nameof(Value),
+        typeof(string),
+        typeof(MetricCard),
+        new PropertyMetadata("0%"));
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+        public static readonly DependencyProperty StatusProperty =
+    DependencyProperty.Register(
+        nameof(Status),
+        typeof(string),
+        typeof(MetricCard),
+        new PropertyMetadata("Normal"));
+
+        public string Status
+        {
+            get => (string)GetValue(StatusProperty);
+            set => SetValue(StatusProperty, value);
+        }
+        public static readonly DependencyProperty ProgressValueProperty =
+    DependencyProperty.Register(
+        nameof(ProgressValue),
+        typeof(double),
+        typeof(MetricCard),
+        new PropertyMetadata(0.0));
+
+        public double ProgressValue
+        {
+            get => (double)GetValue(ProgressValueProperty);
+            set => SetValue(ProgressValueProperty, value);
+        }
+    }
+}
