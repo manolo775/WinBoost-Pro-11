b1badbe7adebf4b2aa0c4617612bb724de1a85bb
manolo775 <mariandobre2000@yahoo.com>
2026-08-13 20:06:01 +0300

    feat: add Windows Update settings shortcut and polish layout

--- src/WinBoost-Pro-11/WinBoost.App/Views/WindowsUpdateView.xaml.cs
+++ src/WinBoost-Pro-11/WinBoost.App/Views/WindowsUpdateView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
 using WinBoost.App.ViewModels;
 
 namespace WinBoost.App.Views
@@ -13,6 +16,29 @@ namespace WinBoost.App.Views
 
             _viewModel = new WindowsUpdateViewModel();
             DataContext = _viewModel;
+        }
+
+        private void OpenWindowsUpdate_Click(
+            object sender,
+            RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(
+                    new ProcessStartInfo
+                    {
+                        FileName = "ms-settings:windowsupdate",
+                        UseShellExecute = true
+                    });
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                    "Windows Update could not be opened.",
+                    "WinBoost Pro 11",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
         }
     }
 }
